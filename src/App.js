af7720bad5e4d337666a5a58bb5a41e6803a0ca6
Liuda Arkavenko <arkavenko18@gmail.com>
2021-06-03 23:37:25 +0300

    try to solve the problem

--- src/App.js
+++ src/App.js
@@ -7,39 +7,37 @@ function App() {
   const[review, setReview]=useState("");
   const [movieReviewList, setMovieList] = useState([]);
   const [updatedReview, setUpdatedReview] = useState("");
-
+  // https://kaven-movie-reviewer.herokuapp.com
   useEffect(()=>{
-    Axios.get('https://kaven-movie-reviewer.herokuapp.com/api/get').then((response)=>{
+    Axios.get('http://localhost:3002/api/get' || 'https://kaven-movie-reviewer.herokuapp.com/api/get').then((response)=>{
         setMovieList(response.data)
     })
   },[])
 
-
-  const submitReview =()=>{
-      Axios.post('https://kaven-movie-reviewer.herokuapp.com/api/insert',{
+  const submitReview =(movie)=>{
+      Axios.post('http://localhost:3002/api/insert' ||'https://kaven-movie-reviewer.herokuapp.com/api/insert',{
         movieName:movieName,
         movieReview:review,
       });
-
         setMovieList([...movieReviewList,
         {movieName: movieName, movieReview:review},
       ]);   
+      console.log("fine")
   };
   const deleteReview=(movie)=>{
-    Axios.delete(`https://kaven-movie-reviewer.herokuapp.com/api/delete/${movie}`)
+    Axios.delete(`http://localhost:3002/api/delete/${movie}`|| 'https://kaven-movie-reviewer.herokuapp.com/api/delete/${movie}')
         setMovieList(movieReviewList.filter((val)=>{
-          console.log(val)
           return movie != val.movieName
-        }))
+      }))
   }
   const updateReview=(movie)=>{
-    Axios.put("https://kaven-movie-reviewer.herokuapp.com/api/update",{movieName:movie, movieReview:updatedReview,})
+    Axios.put("http://localhost:3002/api/update" ||'https://kaven-movie-reviewer.herokuapp.com/api/update',{movieName:movie, movieReview:updatedReview,})
     setMovieList(movieReviewList.map((val)=>{
-      console.log(updatedReview)
-      return movie === val.movieName ? {movieName: val.movieName, movieReview:updatedReview,}:val
+      return movie === val.movieName ? [{movieName: val.movieName, movieReview:updatedReview}]:val
     }))
 
-    }
+  } 
+  console.log(movieReviewList)
   return (
     <div className="App">
       <div className="title">Movie Rewiews</div>
@@ -47,8 +45,8 @@ function App() {
               <input className="movie-name" placeholder="Movie Name..." onChange={(e)=>{setMovieName(e.target.value)}}></input>
               <input className="movie-review" placeholder="Movie Review..." onChange={(e)=>{setReview(e.target.value)}}></input>
               <button className="add-btn" onClick={submitReview}>Add review</button>
-              {movieReviewList.map((val)=>{
-                return (              
+              {movieReviewList.map((val)=>{    
+                return(                       
                   <div className="item"  key={val.id}>
                       <div className="name-item"> <p className="title-item">Movie Name:</p> {val.movieName} </div> 
                       <div className="review-item"><br/> <p className="title-item">Movie Review:</p> {val.movieReview}</div>
@@ -60,9 +58,9 @@ function App() {
                         <button className="delete-btn" onClick={()=>{deleteReview(val.movieName)}}>Delete</button>
                       </div>
                   </div>
-                
                 )
-              })}
+                
+                })}
           </div>
       </div>
   );
